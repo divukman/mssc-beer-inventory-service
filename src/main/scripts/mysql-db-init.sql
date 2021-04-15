ea6ef5266457dbbb2ab469893b904a3443390352
dimitar <dimitar.vukman@bluedropmedical.com>
2021-04-15 18:25:08 +0200

    Refactoring: MySQL local profiles, mysql db init scripts.

---
+++ src/main/scripts/mysql-db-init.sql
@@ -0,0 +1,9 @@
+DROP DATABASE IF EXISTS beer_inventory_service;
+DROP USER IF EXISTS `beer_inventory_service`@`%`;
+
+CREATE DATABASE IF NOT EXISTS beer_inventory_service CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE USER IF NOT EXISTS `beer_inventory_service`@`%` IDENTIFIED WITH mysql_native_password BY 'password';
+
+GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, REFERENCES, INDEX, ALTER, EXECUTE, CREATE VIEW, SHOW VIEW,
+CREATE ROUTINE, ALTER ROUTINE, EVENT, TRIGGER ON `beer_inventory_service`.* TO `beer_inventory_service`@`%`;
+FLUSH PRIVILEGES;
